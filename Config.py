5e75d3b6d305f3a0d8afcc161cc0ce6ecb31f69d
13x13x <153285185+13x13x@users.noreply.github.com>
2024-11-29 11:16:16 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,6 +9,6 @@ class Config:
         "MONGO_URL",
         "mongodb+srv://Puka12:puka12@cluster0.4xmyiyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )
-    USER_ID = int(os.getenv("USER_ID", 6290483448)
+    USER_ID = int(os.getenv("USER_ID", "6290483448")
     RSS_FEED_URL = os.getenv("RSS_FEED_URL", "")  # Replace with actual RSS URL
     MAX_LINKS_PER_BATCH = int(os.getenv("MAX_LINKS_PER_BATCH", 10))
